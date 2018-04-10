f063514b661609295dc2a988d89b723cd35a8ce4
Dima <naumkindp@mail.ru>
2018-04-10 17:46:19 +0300

    Create base layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import './App.scss';
+import Header from './header/Header';
+import Main from './main/Main';
+import './App.css';
 
 const App = () => {
   console.log(process.env.REACT_APP_TMDB_API_KEY);
   return (
-    <div className='asd'>
-      DAUN
+    <div>
+      <Header/>
+      <Main/>
     </div>
   )
 }
- export default App
+ export default App;

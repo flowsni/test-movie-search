81d956f5853cd16f306df75c12d9abf47f10e5ee
Dima <naumkindp@mail.ru>
2018-04-10 15:09:45 +0300

    init commit#2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  console.log(process.env.REACT_APP_TMDB_API_KEY);
+  return (
+    <div className='asd'>
+      DAUN
+    </div>
+  )
 }
-
-export default App;
+ export default App
